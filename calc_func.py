a771ab40b9ead8c04972b8a664b7c920ac28f63d
kajalmia3490 <kajalmia3490@gmail.com>
2026-01-16 12:14:28 +0600

    add string in return

--- calc_func.py
+++ calc_func.py
@@ -3,14 +3,14 @@ def calc(values, operator):
         sumation = 0
         for v in values:
             sumation = sumation + v
-        return sumation
+        return f"Your sumation is: {sumation}"
     elif operator == "-":
         return values[0] - values[1]
     elif operator == "*":
         multiplication = 1
         for v in values:
             multiplication = multiplication * v
-        return multiplication
+        return f"Your multiplication is: {multiplication}"
     elif operator == "/":
         for v in values:
             return values[0] / values[1]
