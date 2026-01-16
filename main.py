8e640491464f468adc6e02d30ef3250055cb7c4f
kajalmia3490 <kajalmia3490@gmail.com>
2026-01-16 11:29:23 +0600

    split method

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import calc_func
 # input from user
 value_from_user = input("Enter the values: ")
 
-# print(type(value_from_user))
+# list value split
 value_list = list(map(int, value_from_user.split()))
 
 print(calc_func.calc(value_list, "+"))
